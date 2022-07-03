54182850f0a4acbb0c32d0f8f69ac714f8a7c817
Temirlan <tema_baike@gmail.com>
2022-07-03 10:43:46 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// Задание 1
+// const arr = [{ id: 1, name: 'Piter'}, { id: 2, name: 'Nina' }];
+
+// const findName = arr.find((item) => item.name === 'Nina');
+
+// console.log(findName.name);
+
+
+
+// Задание 2
+// const arr = [{num: 1, floor: 1, people: 2}, {num: 2, floor: 1, people: 3}, {num: 3, floor: 2, people: 4}, {num: 4, floor: 2, people: 3}, {num: 5, floor: 3, people: 5}];
+
+// const lessThree =[];
+
+// const newArr = arr.forEach(function (item) {
+//   if (item.people < 3) {
+//     lessThree.push(item.num);
+//   }
+// });
+
+// console.log(lessThree);
+
+
+
+// Задание 3
+// const arr = [1, 2, 3, 4, 5];
+// arr.splice(3, 0, 'a', 'b', 'c');
+
+// console.log(arr)
+
+
+
+// Задание 4
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const newArr = arr.slice(3, 5);
+
+// console.log(newArr);
+
+
+
+// Задание 5
+// let mas = [15, 'hello', 'trump', 23, 'world', 999, 176];
+
+// let newMas = mas.join('---');
+
+// console.log(newMas);
